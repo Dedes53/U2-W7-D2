300617ea2818304e1118c54f59c855b4913f5dc6
Dedes <federico.lepore73@gmail.com>
2026-01-20 17:29:12 +0100

    timer

--- Timer/script.js
+++ Timer/script.js
@@ -1,5 +1,15 @@
 const timer = document.getElementById('timer');
+const savedTime = "savedTime";
 let time = 0;
+
+// verifichiamo se ci sia o meno del tempo già trasorso, se si lo impostiamo come base da cui partire
+
+if (sessionStorage.getItem(savedTime) === null) { time = 0; }
+else {
+    time = isNaN(parseInt(sessionStorage.getItem(savedTime))) ? 0 : parseInt(sessionStorage.getItem(savedTime));
+}
+
+
 
 setInterval(() => {
     time++; //incremento il time di uno ad ogni esecuzione
@@ -7,5 +17,5 @@ setInterval(() => {
         `<p style="font-size: 2em;">You are watching <b>nothing</b> for </p>
         <h1 style="font-size: 5em; color:red">${time}</h1> 
         <p style="font-size: 2em;">seconds</p>`;
-    sessionStorage.setItem('watchTime', time); // salvo il tempo che trascorre nel sessionStorage 
+    sessionStorage.setItem(savedTime, time); // salvo il tempo che trascorre nel sessionStorage 
 }, 1000); // setto l'eseuzione ad ogni secondo (1000 ms)
