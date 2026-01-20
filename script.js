225e5414f8aba36fb03a0aa3183e8a9e693987d7
Dedes <federico.lepore73@gmail.com>
2026-01-20 14:51:36 +0100

    timer

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let time = 0;
 
 setInterval(() => {
     time++;
-    timer.innerHTML = `<p style="font-size: 2em;">You are watching nothing for </p>
+    timer.innerHTML = `<p style="font-size: 2em;">You are watching <b>nothing</b> for </p>
    <h1 style="font-size: 5em; color:red">${time}</h1> 
    <p style="font-size: 2em;">seconds</p>`;
     sessionStorage.setItem('watchTime', time);
