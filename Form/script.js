196ba0b72143f9cd27167f8f2c61915bc7b951db
Dedes <federico.lepore73@gmail.com>
2026-01-20 15:44:42 +0100

    comment

--- Form/script.js
+++ Form/script.js
@@ -5,8 +5,9 @@ const cancel = document.getElementById('cancel');
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const id = document.getElementById('id');
-    const user = id.value;
+    const id = document.getElementById('id'); // riferimento al campo input
+    const user = id.value; // leggiamo e assegnamo il valore del campo input ad una variabile
 
+    sessionStorage.setItem("user", user); // salviamo il valore del campo input come variabile nel sessionStorage
 
 });
