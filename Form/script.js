cfa9cfbfc511ce3feb40b37f2445af9bbef6a3c9
Dedes <federico.lepore73@gmail.com>
2026-01-20 16:03:01 +0100

    cancel event

--- Form/script.js
+++ Form/script.js
@@ -9,8 +9,11 @@ form.addEventListener('submit', function (event) {
     const user = id.value; // leggiamo e assegnamo il valore del campo input ad una variabile
 
     sessionStorage.setItem(savedUser, user); // salviamo il valore del campo input come variabile nel sessionStorage
+
+    form.reset(); // resettiamo il form
 });
 
 cancel.addEventListener('click', function () {
-
+    sessionStorage.removeItem(savedUser);
 })
+
