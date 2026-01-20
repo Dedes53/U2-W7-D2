a7095132e66c8f5a019dc244f34ffcc71f7a6c94
Dedes <federico.lepore73@gmail.com>
2026-01-20 15:39:52 +0100

    form event

--- Form/script.js
+++ Form/script.js
@@ -2,3 +2,11 @@ const form = document.getElementById('form');
 // const save = document.getElementById('save');
 const cancel = document.getElementById('cancel');
 
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const id = document.getElementById('id');
+    const user = id.value;
+
+
+});
